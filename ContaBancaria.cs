b3715633f0c590138604fbd88ddf66ae2e871e1d
brunossilva12-creator <bruno.s.silva12@edu.senai.br>
2026-08-28 09:41:08 -0300

    Atividades abstração

---
+++ ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Abstracao
+{
+    public class ContaBancaria
+    {
+        public string Titular;
+        public string NumeroConta;
+        public double Saldo;
+        public double Deposito;
+        public double Saque;
+
+        public void Depositar()
+        {
+            Console.WriteLine($"Titular: {Titular}, Número da conta: {NumeroConta}");
+            Console.WriteLine($"Seu saldo incial: {Saldo}, Seu depósito: {Deposito}, Seu saldo atual {Saldo + Deposito:F2}");
+            Console.WriteLine("------------------------");
+        }
+
+        public void Sacar()
+        {
+            Console.WriteLine($"Titular: {Titular}, Número da conta: {NumeroConta}");
+
+            if (Saque <= Saldo)
+            {
+                Console.WriteLine($"Seu saldo incial: {Saldo}, Seu valor Sacado {Saque}, Seu saldo atual {Saldo - Saque:F2}");
+                Console.WriteLine("------------------------");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente, deseja fazer um empréstimo bancário? (não responda)");
+                Console.WriteLine("------------------------");
+            }
+        }
+    }
+}
