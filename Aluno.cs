8e97837febb1080032b818a9ca892801f10d3be7
brunossilva12-creator <bruno.s.silva12@edu.senai.br>
2026-08-28 09:15:20 -0300

    Atividade 2 (E atividade 1 editada)

---
+++ Aluno.cs
@@ -0,0 +1,16 @@
+﻿namespace Abstracao
+{
+    public class Aluno
+    {
+        public string Nome;
+        public int Matricula;
+        public double Nota1;
+        public double Nota2;
+
+        public void ExibirInformacoesAluno()
+        {
+            Console.WriteLine($"Nome: {Nome}, Registro de matrícula: {Matricula}");
+            Console.WriteLine($"Nota 1: {Nota1}, Nota 2: {Nota2}, Media: {(Nota1 + Nota2) / 2}");
+        }
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace Abstracao
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int QuantidadeEstoque;
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Produto: {Nome}");
+            Console.WriteLine($"Preço: {Preco}");
+            Console.WriteLine($"Quantidade em estoque {QuantidadeEstoque}");
+            Console.WriteLine("------------------------");
+        }
+    }
+}
