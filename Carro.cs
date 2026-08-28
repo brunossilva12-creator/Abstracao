27c44d632ebc4ac03f5a293dcc4889199aa512c9
brunossilva12-creator <bruno.s.silva12@edu.senai.br>
2026-08-28 08:29:07 -0300

    Adicionar arquivos de projeto.

---
+++ Carro.cs
@@ -0,0 +1,19 @@
+﻿namespace Abstracao
+{
+    public class Carro
+    {
+        //ATRIBUTOS (CARACTERÍSTICAS)
+        public string Marca;
+        public string Modelo;
+        public int Ano;
+        public string Cor;
+
+        //MÉTODO (COMPORTAMENTO)
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Carro: {Marca} {Modelo}");
+            Console.WriteLine($"Cor: {Cor}, Ano: {Ano}");
+            Console.WriteLine("------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Abstracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CRIANDO O OBJETO (INSTANCIAÇÃO)
+            Carro MeuFusca = new Carro();
+
+            MeuFusca.Marca = "Volkswagen";
+            MeuFusca.Modelo = "Fusca";
+            MeuFusca.Ano = 1975;
+            MeuFusca.Cor = "Azul";
+
+            Carro CarroVizinho = new Carro();
+
+            CarroVizinho.Marca = "Fiat";
+            CarroVizinho.Modelo = "Uno";
+            CarroVizinho.Ano = 2010;
+            CarroVizinho.Cor = "Prata";
+
+            Console.WriteLine("Informações do meu carro: ");
+            MeuFusca.ExibirInformacoes();
+
+            Console.WriteLine("Informações do carro do meu vizinho: ");
+            CarroVizinho.ExibirInformacoes();
+
+
+            //EXERCÍCIO 1
+
+            Produtos Produto1 = new Produtos();
+            Produto1.Nome = "Caneta";
+            Produto1.Preco = 3.99;
+            Produto1.QuantidadeEstoque = 500;
+
+            Console.WriteLine("Informações do produto: ");
+            Produto1.ExibirDados();
+
+            Produtos Produto2 = new Produtos();
+            Produto2.Nome = "Caderno";
+            Produto2.Preco = 39.99;
+            Produto2.QuantidadeEstoque = 200;
+
+            Console.WriteLine("Informações do produto: ");
+            Produto2.ExibirDados();
+
+            Console.ReadKey();
+        }
+    }
+}
